2fb8a1e8c07bff0da7f33c7847d901131288867e
Chris Bitsakis <c.bitsakis@gmail.com>
2023-04-18 15:36:30 -0400

    netlify email function, wip

--- astro.i18n.config.ts
+++ astro.i18n.config.ts
@@ -1,96 +1,102 @@
-import { defineAstroI18nConfig } from "astro-i18n"
+import { defineAstroI18nConfig } from 'astro-i18n'
 
 const en = {
-  siteName: "La Well Piétonne",
-  siteDescription: "A public consultation and petition to pedestrianize Wellington street permanently",
+  siteName: 'La Well Piétonne',
+  siteDescription: 'A public consultation and petition to pedestrianize Wellington street permanently',
 
   otherLangs: {
-    fr: "Français"
+    fr: 'Français'
   },
 
-  headerDescription: "A public consultation to make Wellington a pedestrian street permanently",
+  headerDescription: 'A public consultation to make Wellington a pedestrian street permanently',
 
-  stepsTitle: "Initiative progress",
+  stepsTitle: 'Initiative progress',
   steps: {
     1: {
-      title: "Petition draft",
-      subtitle: "Signed by 25 residents"
+      title: 'Petition draft',
+      subtitle: 'Signed by 25 residents'
     },
     2: {
-      title: "Petition text approved",
-      subtitle: "by the borough of Verdun"
+      title: 'Petition text approved',
+      subtitle: 'by the borough of Verdun'
     },
     3: {
-      title: "Signature period",
-      subtitle: "90 days - 2500 signatures"
+      title: 'Signature period',
+      subtitle: '90 days ~ 2500 signatures'
     },
     4: {
-      title: "Public consultation",
-      subtitle: "with residents of Verdun"
+      title: 'Public consultation',
+      subtitle: 'with residents of Verdun'
     }
   },
 
-  petitionTextHeading: "Petition text",
+  petitionTextHeading: 'Petition text',
 
-  signCta: "Sign the petition",
-  signCtaSubtext: "on montreal.ca",
-  signCtaRequirements: "Must be a resident of Verdun age 15 or older",
+  signCta: 'Sign the petition',
+  signCtaSubtext: 'on montreal.ca',
+  signCtaRequirements: 'Must be a resident of Verdun age 15 or older',
 
-  volunteerCta: "Volunteer",
-  volunteerCtaSubtext: "for signature collection",
+  volunteerCta: 'Volunteer',
+  volunteerCtaSubtext: 'for signature collection',
 
-  inspirationAndReading: "Inspiration and reading",
+  inspirationAndReading: 'Inspiration and reading',
   
-  footerNoticeUnaffiliated: "This public initiative is not affiliated with ",
+  footerNoticeUnaffiliated: 'This public initiative is not affiliated with ',
+
+  petitionUrl: 'https://montreal.ca/en/petitions/details/6428dc4ac490e6ee4229c32a'
 }
 
 const fr = {
-  siteName: "La Well piétonne",
-  siteDescription: "Consultation publique et pétition pour la piétonnisation permanente de la rue Wellington",
+  siteName: 'La Well piétonne',
+  siteDescription: 'Consultation publique et pétition pour la piétonnisation permanente de la rue Wellington',
 
   otherLangs: {
-    en: "English"
+    en: 'English'
   },
 
-  headerDescription: "Une consultation publique pour piétonniser la rue Wellington en permanence",
+  headerDescription: 'Une consultation publique pour piétonniser la rue Wellington en permanence',
 
-  stepsTitle: "Progrès de l'initiative",
+  stepsTitle: 'Progrès de l\'initiative',
   steps: {
     1: {
-      title: "Projet de pétition",
-      subtitle: "Signée par 25 résidents"
+      title: 'Projet de pétition',
+      subtitle: 'Signée par 25 résidents'
     },
     2: {
-      title: "Texte approuvé",
-      subtitle: "par l'arrondissement de Verdun"
+      title: 'Texte approuvé',
+      subtitle: 'par l\'arrondissement de Verdun'
     },
     3: {
-      title: "Période de signatures",
-      subtitle: "90 jours - 2500 signatures"
+      title: 'Période de signatures',
+      subtitle: '90 jours ~ 2500 signatures'
     },
     4: {
-      title: "Consultation publique",
-      subtitle: "avec les résidents de Verdun"
+      title: 'Consultation publique',
+      subtitle: 'avec les résidents de Verdun'
     }
   },
 
-  petitionTextHeading: "La texte du pétition",
+  petitionTextHeading: 'La texte du pétition',
 
-  signCta: "Signer la pétition",
-  signCtaSubtext: "sur montreal.ca",
-  signCtaRequirements: "Résidents de Verdun âgé de 15 ans ou plus",
+  signCta: 'Signer la pétition',
+  signCtaSubtext: 'sur montreal.ca',
+  signCtaRequirements: 'Résidents de Verdun âgé de 15 ans ou plus',
 
-  volunteerCta: "Bénévole",
-  volunteerCtaSubtext: "pour la collecte de signatures",
+  volunteerCta: 'Bénévole',
+  volunteerCtaSubtext: 'pour la collecte de signatures',
 
-  inspirationAndReading: "Inspiration et lecture",
+  inspirationAndReading: 'Inspiration et lecture',
 
-  footerNoticeUnaffiliated: "Cette initiative publique n'est pas affiliée à ",
+  footerNoticeUnaffiliated: 'Cette initiative publique n\'est pas affiliée à ',
+
+  petitionUrl: 'https://montreal.ca/petitions/detail/6428dc4ac490e6ee4229c32a'
 }
 
+export const translations = { en, fr }
+
 export default defineAstroI18nConfig({
-  defaultLangCode: "fr",
-  supportedLangCodes: ["en"],
+  defaultLangCode: 'fr',
+  supportedLangCodes: ['en'],
   showDefaultLangCode: true,
   translations: { en, fr },
   routeTranslations: {},
---
+++ netlify/functions/register/register.ts
@@ -0,0 +1,65 @@
+import { Handler } from '@netlify/functions'
+import { translations } from '../../../astro.i18n.config'
+import { StatusCodes } from 'http-status-codes'
+
+export const handler: Handler = async ({ body }) => {
+  try {
+    const { email, lang = 'fr' } = JSON.parse(body)
+    
+    const montrealRegistration = 'https://api.montreal.ca/api/it-platforms/security/v1/tickets/registration'
+    const data = {
+      email,
+      language: lang,
+      redirectUri: translations[lang].petitionUrl
+    }
+
+    // Register email address on montreal.ca
+    const response = await fetch(montrealRegistration, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+
+    const responseJson = await response.json()
+
+    // Invalid email
+    const invalidEmailError = responseJson.error?.code === 'invalidParameter'
+    if (invalidEmailError) {
+    // TODO validation, no pluses
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+      }
+    }
+  
+    // Email already has account on montreal.ca
+    // Instruct user to proceed with petition signing
+    const hasMontrealAccount = responseJson.error?.code === 'accountAlreadyExists'
+    if (hasMontrealAccount) {
+      return {
+        statusCode: StatusCodes.OK,
+      }
+    }
+  
+    // Account created on montreal.ca
+    // Instruct user to proceed via email confirmation from montreal.ca
+    const accountCreated = responseJson.email
+    if (accountCreated) {
+      return {
+        statusCode: StatusCodes.CREATED,
+      }
+    }
+
+    // Else unknown error
+    // Fail silently by redirecting to the petition
+    throw new Error(JSON.stringify(responseJson))
+  } catch (error) {
+    return {
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      body: JSON.stringify({
+        error
+      })
+    }
+  }
+}
